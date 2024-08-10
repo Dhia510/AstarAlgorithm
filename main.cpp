ac6bcf8b891a30050954ba34b8183675e9237eff
Dhia510 <dhia.chaabene123@gmail.com>
2024-08-10 18:17:38 +0000

    main.cpp: define the board vector and a print function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,31 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
 
+// TODO: Add PrintBoard function here.
+static void PrintBoard(vector<vector<int>> p_vector);
+  
+  
 int main() {
-
+  vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
+                            {0, 1, 0, 0, 0, 0},
+                            {0, 1, 0, 0, 0, 0},
+                            {0, 1, 0, 0, 0, 0},
+                            {0, 0, 0, 0, 1, 0}};
+  // TODO: Call PrintBoard function here.
+  PrintBoard(board);
 }
+
+static void PrintBoard(vector<vector<int>> p_vector)
+{
+  for(auto l_v : p_vector)
+  {
+    for(int i : l_v)
+    {
+      cout << i;
+    }
+    
+    cout << "\n";
+  }
+}  
